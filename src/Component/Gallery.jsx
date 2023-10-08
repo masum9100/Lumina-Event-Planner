91f2d9550d3d05097f81247e4cffa79fc59f2c62
masum9100 <masum.seopro@gmail.com>
2023-10-08 17:17:23 +0600

    About Us and Gallery page completed

--- src/Component/Gallery.jsx
+++ src/Component/Gallery.jsx
@@ -3,7 +3,29 @@
 const Gallery = () => {
     return (
         <div>
-            photo
+            <div className="flex flex-wrap gap-2 justify-center">
+                <img src="./1.jpg" alt="" />
+                <img src="./2.jpg" alt="" />
+                <img src="./3.jpg" alt="" />
+                <img src="./4.jpg" alt="" />
+                <img src="./5.jpg" alt="" />
+                <img src="./6.jpg" alt="" />
+                <img src="./7.jpg" alt="" />
+                <img src="./8.jpg" alt="" />
+                <img src="./9.jpg" alt="" />
+                <img src="./10.jpg" alt="" />
+                <img src="./11.jpg" alt="" />
+                <img src="./12.jpg" alt="" />
+                <img src="./13.jpg" alt="" />
+                <img src="./14.jpg" alt="" />
+                <img src="./15.jpg" alt="" />
+                <img src="./16.jpg" alt="" />
+                <img src="./17.jpg" alt="" />
+                <img src="./18.jpg" alt="" />
+                <img src="./19.jpg" alt="" />
+                <img src="./20.jpg" alt="" />
+                <img src="./21.jpg" alt="" />
+            </div>
         </div>
     );
 };
